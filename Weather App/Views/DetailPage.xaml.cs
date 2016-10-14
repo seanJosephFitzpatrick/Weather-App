ad1c8bbcc678efcdd9fa280680b15411b1196a1a
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2016-10-14 22:54:17 +0100

    Windows Template 10

---
+++ Weather App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Weather_App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Weather_App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
